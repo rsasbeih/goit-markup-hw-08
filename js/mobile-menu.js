d08d4e894b10c062f0508decf7dc51ff30bb8bfb
Ruba Sbeih <rsasbeih@gmail.com>
2022-09-26 21:25:15 +0300

    mobile menu added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,20 +1,14 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-
-  const toggleMenu = () => {
-    const isMenuOpen =
-    mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
+    const refs = {
+      openMenuBtn: document.querySelector("[data-menu-open]"),
+      closeMenuBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenuBtn.addEventListener("click", toggleModal);
+    refs.closeMenuBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.menu.classList.toggle("is-open");
+    }
+  })();
